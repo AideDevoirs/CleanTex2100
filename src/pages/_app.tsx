d661b0eefb5cf5cf2e925a20536c5cbc14d82f83
eliasZemmouri <elias.zemmouri@student.vinci.be>
2024-11-24 15:05:43 +0100

    logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
+/* eslint-disable */
 import { AppProps } from 'next/app';
+import Head from 'next/head'; // Importez Head pour gérer les balises <head>
+
 
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    {/* Configuration globale du <head> */}
+    <Head>
+      <link rel="icon" href="/assets/images/ClLogo.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
